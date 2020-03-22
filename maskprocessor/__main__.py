af7f847d0b14acfbb8f70e1f81be102a2d9ea696
Xvezda <xvezda@naver.com>
2020-03-22 09:32:48 +0900

    Add __main__.py

---
+++ maskprocessor/__main__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020 Xvezda <xvezda@naver.com>
+#
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from . import main
+
+
+if __name__ == '__main__':
+    main()
+
